e3337a703752a77000a68ee0a8705ff0b4f746a3
creepersaur <69436784+creepersaur@users.noreply.github.com>
2025-07-17 17:50:07 +0530

    idk

--- example.js
+++ example.js
@@ -14,28 +14,6 @@ export default new class SeaDex extends AbstractSource {
         leechers: 12,
         accuracy: 'low',
         downloads: 102203
-      },
-      {
-        hash: 'c9e15763f722f23e98a29decdfae341b98d53056',
-        link: 'c9e15763f722f23e98a29decdfae341b98d53056',
-        title: 'Cosmos Laundromat',
-        size: 276000000,
-        date: new Date('2017-03-30T23:30:17'),
-        seeders: 1401,
-        leechers: 12,
-        accuracy: 'low',
-        downloads: 102203
-      },
-      {
-        hash: '08ada5a7a6183aae1e09d831df6748d566095a10',
-        link: '08ada5a7a6183aae1e09d831df6748d566095a10',
-        title: 'Sintel',
-        size: 276000000,
-        date: new Date('2017-03-30T23:30:17'),
-        seeders: 1401,
-        leechers: 12,
-        accuracy: 'low',
-        downloads: 102203,
       }
     ]
   }
